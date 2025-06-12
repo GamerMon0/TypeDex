261b1602b6f85a90155f50a2bc350050fd537178
GamerMon0 <monsterongamer24@gmail.com>
2025-06-12 12:18:52 +0200

    changed the pokemon data from default class constructor to new constructor

--- data.cpp
+++ data.cpp
@@ -5,31 +5,11 @@
 std::vector<Pokemon> allPokemon;
 
 void loadPokemonData(){
-    Pokemon bulbasaur;
-    bulbasaur.species = "Bulbasaur";
-    bulbasaur.type = "Grass Poison";
-    bulbasaur.number = 1;
-    bulbasaur.hp = 45;
-    bulbasaur.attack = 49;
-    bulbasaur.defense = 49;
-    bulbasaur.specialattack = 65;
-    bulbasaur.specialdefense = 65;
-    bulbasaur.speed = 45;
-    bulbasaur.total = 318;
-
+    Pokemon bulbasaur("Bulbasaur", "Grass Poison", 1, 45, 49, 49, 65, 65, 45, 318);
+    
     allPokemon.push_back(bulbasaur);
 
-    Pokemon ivysaur;
-    ivysaur.species = "Ivysaur";
-    ivysaur.type = "Grass Poison";
-    ivysaur.number = 2;
-    ivysaur.hp = 60;
-    ivysaur.attack = 62;
-    ivysaur.defense = 63;
-    ivysaur.specialattack = 80;
-    ivysaur.specialdefense = 80;
-    ivysaur.speed = 60;
-    ivysaur.total = 405;
+    Pokemon ivysaur("Ivysaur", "Grass Poison", 2, 60, 62, 63, 80, 80, 60, 405);
 
     allPokemon.push_back(ivysaur);
 
