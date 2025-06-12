edf0c99c46a97d7178b4c2a9ccadc86c56249dba
GamerMon0 <monsterongamer24@gmail.com>
2025-06-12 12:17:14 +0200

    added a new constructor for the Pokemon class

--- pokemon.h
+++ pokemon.h
@@ -17,6 +17,16 @@ class Pokemon{
         int speed;
         int total;
 
+     Pokemon(std::string species, std::string type, int number,
+            int hp, int attack, int defense, int specialattack,
+            int specialdefense, int speed, int total){
+
+            this->species = species; this->type = type; this->number = number;
+            this->hp = hp; this->attack = attack; this->defense = defense;
+            this->specialattack = specialattack; this-> specialdefense = specialdefense;
+            this->speed = speed; this ->total = total;
+    }
+
         void weak(); // for example: weak bulbasaur = grass poison type : weak to: fire flying ice psychic, resistant to: figthing water grass electric fairy, neutral to: normal poison ground rock bug ghjost steel dragon dark.
         void stats(); // for example stats bulbasaur = hp: 45, attack: 49, defense: 49, sp. attack: 65, sp. defense: 65, speed: 45 
 };
