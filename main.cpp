d03fc4c6107cba22ef0f558a5371d75b51e8e036
GamerMon0 <monsterongamer24@gmail.com>
2025-06-12 12:22:04 +0200

    added the int main function and just tried printing some tests in the console, nothing major

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
 // main menu lgoic and all
+
+#include <iostream>
+#include "pokemon.h"
+#include "data.h"
+
+void loadPokemonData();
+
+int main(){
+
+    Pokemon pikachu("Pikachu", "Electric", 25, 1, 2, 3, 4, 5, 6, 7);
+
+
+    
+    std::cout << allPokemon[0].species << std::endl;
+
+
+
+
+
+    return 0;
+}
